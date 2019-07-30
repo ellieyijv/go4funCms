dafa8c0c97a5ab01b029b166a1c10c7dc46b4bcc
ellieyijv <yijv2013@gmail.com>
2019-07-30 10:47:53 +1000

    ext-gd temparory

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,9 @@ class Controller extends BaseController
 
     protected function jsonResponse($data, $code = 200)
     {
-        return response()->json($data, $code,
+        return response()->json(
+            ['data' => $data], 
+            $code,
             ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
     }
 }
